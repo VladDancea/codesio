7e89fe15c8e9dcfe62e04219a58e9544d2f41e8b
dancea <e12123455@student.tuwien.ac.at>
2024-12-23 13:56:29 +0200

    Modify base path for vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/codesio/"
-})
+  base: "./", // Use a relative path for local testing
+});
